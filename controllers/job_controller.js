a8ee4a767593cb4320fe59aa5ed1b6b2d200404e
AbdulWadood2 <abdulwadoodowner@gmail.com>
2024-08-10 14:22:32 +0500

    job api's are done

--- controllers/job_controller.js
+++ controllers/job_controller.js
@@ -123,4 +123,217 @@ const createJob = catchAsync(async (req, res, next) => {
   );
 });
 
-module.exports = { createJob };
+// method GET
+// endPoint /api/v1/job
+// description get job
+const getJobs = catchAsync(async (req, res, next) => {
+  const { page = 1, limit = 10 } = req.query;
+
+  // Calculate the offset
+  const skip = (page - 1) * limit;
+
+  // Fetch the jobs with pagination
+  let jobs = await job_model
+    .find({ employerId: req.user.id })
+    .skip(skip)
+    .limit(limit)
+    .sort({ createdAt: -1 }); // Assuming you want the newest jobs first
+
+  // Get the total number of jobs to calculate the total pages
+  const totalJobs = await job_model.countDocuments({ employerId: req.user.id });
+  const totalPages = Math.ceil(totalJobs / limit);
+  jobs = await Promise.all(
+    jobs.map(async (job) => {
+      [job.specification.video] = await generateSignedUrl([
+        job.specification.video,
+      ]);
+      [job.specification.docs] = await generateSignedUrl([
+        job.specification.docs,
+      ]);
+      [job.training.video] = await generateSignedUrl([job.training.video]);
+      [job.training.docs] = await generateSignedUrl([job.training.docs]);
+      [job.contract.video] = await generateSignedUrl([job.contract.video]);
+      return job;
+    })
+  );
+  // Return paginated results
+  return successMessage(200, res, "Jobs fetched successfully", {
+    jobs,
+    pagination: {
+      currentPage: Number(page),
+      totalPages,
+      totalJobs,
+    },
+  });
+});
+
+// method GET
+// endPoint /api/v1/job/:id
+// description get job by id
+const getJobById = catchAsync(async (req, res, next) => {
+  const jobId = req.params.id;
+  const job = await job_model.findOne({ _id: jobId, employerId: req.user.id });
+  if (!job) {
+    return next(
+      new appError(` Job not found with id ${jobId}. Please try again!`, 400)
+    );
+  }
+  [job.specification.video] = await generateSignedUrl([
+    job.specification.video,
+  ]);
+  [job.specification.docs] = await generateSignedUrl([job.specification.docs]);
+  [job.training.video] = await generateSignedUrl([job.training.video]);
+  [job.training.docs] = await generateSignedUrl([job.training.docs]);
+  [job.contract.video] = await generateSignedUrl([job.contract.video]);
+  return successMessage(202, res, `job fetched successfully`, job);
+});
+
+// method PUT
+// endPoint /api/v1/job/:id
+// description edit the job
+const editJob = catchAsync(async (req, res, next) => {
+  const jobId = req.params.id;
+  const { error, value } = jobValidationSchema.validate(req.body);
+  if (error) {
+    const errors = error.details.map((detail) => detail.message).join(",");
+    return next(new appError(errors, 400));
+  }
+  [value.specification.video] = await getFileName([value.specification.video]);
+  [value.specification.docs] = await getFileName([value.specification.docs]);
+  [value.training.video] = await getFileName([value.training.video]);
+  [value.training.docs] = await getFileName([value.training.docs]);
+  [value.contract.video] = await getFileName([value.contract.video]);
+  const previousJob = await job_model.findOne({
+    _id: jobId,
+    employerId: req.user.id,
+  });
+  if (!previousJob) {
+    return next(new appError(`Job not found`, 400));
+  }
+  const checks = [];
+
+  if (previousJob.specification.video !== value.specification.video) {
+    checks.push(
+      checkImageExists([value.specification.video])
+        .then((exist) => {
+          if (!exist[0]) {
+            throw new appError(`Specification Video not found`, 400);
+          }
+          return checkDuplicateAwsImgsInRecords([value.specification.video]);
+        })
+        .then((result) => {
+          if (!result.success) {
+            throw new appError(`Specification Video already Used`, 400);
+          }
+        })
+    );
+  }
+
+  if (previousJob.specification.docs !== value.specification.docs) {
+    checks.push(
+      checkImageExists([value.specification.docs])
+        .then((exist) => {
+          if (!exist[0]) {
+            throw new appError(`Specification Docs not found`, 400);
+          }
+          return checkDuplicateAwsImgsInRecords([value.specification.docs]);
+        })
+        .then((result) => {
+          if (!result.success) {
+            throw new appError(`Specification Docs already Used`, 400);
+          }
+        })
+    );
+  }
+
+  if (previousJob.training.video !== value.training.video) {
+    checks.push(
+      checkImageExists([value.training.video])
+        .then((exist) => {
+          if (!exist[0]) {
+            throw new appError(`Training Video not found`, 400);
+          }
+          return checkDuplicateAwsImgsInRecords([value.training.video]);
+        })
+        .then((result) => {
+          if (!result.success) {
+            throw new appError(`Training Video already Used`, 400);
+          }
+        })
+    );
+  }
+
+  if (previousJob.training.docs !== value.training.docs) {
+    checks.push(
+      checkImageExists([value.training.docs])
+        .then((exist) => {
+          if (!exist[0]) {
+            throw new appError(`Training Docs not found`, 400);
+          }
+          return checkDuplicateAwsImgsInRecords([value.training.docs]);
+        })
+        .then((result) => {
+          if (!result.success) {
+            throw new appError(`Training Docs already Used`, 400);
+          }
+        })
+    );
+  }
+
+  if (previousJob.contract.video !== value.contract.video) {
+    checks.push(
+      checkImageExists([value.contract.video])
+        .then((exist) => {
+          if (!exist[0]) {
+            throw new appError(`Contract Video not found`, 400);
+          }
+          return checkDuplicateAwsImgsInRecords([value.contract.video]);
+        })
+        .then((result) => {
+          if (!result.success) {
+            throw new appError(`Contract Video already Used`, 400);
+          }
+        })
+    );
+  }
+
+  await Promise.all(checks);
+
+  const testBuilder = await textBuilder_model.findOne({
+    _id: value.testBuilderId,
+  });
+  if (!testBuilder) {
+    return next(new appError("Test Builder not found", 400));
+  }
+
+  const job = await job_model.findByIdAndUpdate(
+    { _id: jobId },
+    { $set: value },
+    { new: true }
+  );
+  [job.specification.video] = await generateSignedUrl([
+    job.specification.video,
+  ]);
+  [job.specification.docs] = await generateSignedUrl([job.specification.docs]);
+  [job.training.video] = await generateSignedUrl([job.training.video]);
+  [job.training.docs] = await generateSignedUrl([job.training.docs]);
+  [job.contract.video] = await generateSignedUrl([job.contract.video]);
+  return successMessage(202, res, `job updated successfully`, job);
+});
+
+// method DELETE
+// endPoint /api/v1/job/:id
+// description delete the job
+const deleteJob = catchAsync(async (req, res, next) => {
+  const jobId = req.params.id;
+  const job = await job_model.findOneAndDelete({
+    _id: jobId,
+    employerId: req.user.id,
+  });
+  if (!job) {
+    return next(new appError("Job not found", 400));
+  }
+  return successMessage(202, res, `job deleted successfully`);
+});
+
+module.exports = { createJob, getJobs, getJobById, editJob, deleteJob };
--- routes/job_routes.js
+++ routes/job_routes.js
@@ -1,5 +1,11 @@
 const express = require("express");
-const { createJob } = require("../controllers/job_controller");
+const {
+  createJob,
+  getJobs,
+  getJobById,
+  editJob,
+  deleteJob,
+} = require("../controllers/job_controller");
 const employer_model = require("../models/employer_model");
 const { verifyToken } = require("../authorization/verifyToken");
 const route = express.Router();
@@ -102,5 +108,179 @@ const route = express.Router();
  *         description: Job created successfully
  */
 route.post("/", verifyToken([employer_model]), createJob);
+
+/**
+ * @swagger
+ * /api/v1/job:
+ *   get:
+ *     summary: Get jobs with pagination
+ *     description: Fetch a list of jobs created by the logged-in employer with pagination. The jobs are returned in descending order based on creation date.
+ *     tags:
+ *       - Employer/Jobs
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: query
+ *         name: page
+ *         schema:
+ *           type: integer
+ *           default: 1
+ *         description: The page number to fetch.
+ *       - in: query
+ *         name: limit
+ *         schema:
+ *           type: integer
+ *           default: 10
+ *         description: The number of jobs to fetch per page.
+ *     responses:
+ *       200:
+ *         description: Jobs fetched successfully.
+ */
+route.get("/", verifyToken([employer_model]), getJobs);
+
+/**
+ * @swagger
+ * /api/v1/job/{id}:
+ *   get:
+ *     summary: Get a job by ID
+ *     description: Fetch a specific job by its ID for the logged-in employer. The job's specification, training, and contract media files will include signed URLs.
+ *     tags:
+ *       - Employer/Jobs
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the job to fetch.
+ *     responses:
+ *       202:
+ *         description: Job fetched successfully.
+ */
+route.get("/:id", verifyToken([employer_model]), getJobById);
+
+/**
+ * @swagger
+ * /api/v1/job/{id}:
+ *   put:
+ *     summary: Edit a job by ID
+ *     description: Update the details of a specific job by its ID for the logged-in employer. The updated job will include signed URLs for the specification, training, and contract media files.
+ *     tags:
+ *       - Employer/Jobs
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the job to edit.
+ *     requestBody:
+ *       required: true
+ *       content:
+ *         application/json:
+ *           schema:
+ *             type: object
+ *             properties:
+ *               title:
+ *                 type: string
+ *                 description: The title of the job
+ *                 example: Senior Software Engineer
+ *               specification:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                     example: "Training Material"
+ *                     description: "Title of the training material."
+ *                   video:
+ *                     type: string
+ *                     description: Filename of the specification video
+ *                     example: specification-video.mp4
+ *                   docs:
+ *                     type: string
+ *                     description: Filename of the specification document
+ *                     example: specification-docs.pdf
+ *               training:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                     example: "Training Material"
+ *                     description: "Title of the training material."
+ *                   video:
+ *                     type: string
+ *                     description: Filename of the training video
+ *                     example: training-video.mp4
+ *                   docs:
+ *                     type: string
+ *                     description: Filename of the training document
+ *                     example: training-docs.pdf
+ *               contract:
+ *                 type: object
+ *                 properties:
+ *                   title:
+ *                     type: string
+ *                     example: "Training Material"
+ *                     description: "Title of the training material."
+ *                   video:
+ *                     type: string
+ *                     description: Filename of the contract video
+ *                     example: contract-video.mp4
+ *               testBuilderId:
+ *                  type: string
+ *                  example: "60d9f8f5d5f9c6001c8e4f2a"
+ *                  description: "ID of the associated test builder."
+ *               coverLetter:
+ *                  type: boolean
+ *                  default: true
+ *                  description: "Indicates if a cover letter is required."
+ *               cv:
+ *                  type: boolean
+ *                  default: true
+ *                  description: "Indicates if a CV is required."
+ *               aboutVideo:
+ *                  type: boolean
+ *                  default: true
+ *                  description: "Indicates if an 'about' video is required."
+ *               status:
+ *                  type: boolean
+ *                  default: true
+ *                  description: "Indicates if status."
+ *               privateOrPublic:
+ *                  type: boolean
+ *                  default: true
+ *                  description: "Indicates if privateOrPublic."
+ *     responses:
+ *       202:
+ *         description: Job updated successfully.
+ */
+route.put("/:id", verifyToken([employer_model]), editJob);
+
+/**
+ * @swagger
+ * /api/v1/job/{id}:
+ *   delete:
+ *     summary: Delete a job by ID
+ *     description: Delete a specific job by its ID for the logged-in employer. If the job is not found, an error will be returned.
+ *     tags:
+ *       - Employer/Jobs
+ *     security:
+ *       - bearerAuth: []
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: The ID of the job to delete.
+ *     responses:
+ *       202:
+ *         description: Job deleted successfully.
+ */
+route.delete("/:id", verifyToken([employer_model]), deleteJob);
 
 module.exports = route;
